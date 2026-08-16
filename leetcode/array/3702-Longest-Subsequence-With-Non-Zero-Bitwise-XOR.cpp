7f108f6e1642ce088ee4f58e2cefb5ff741e0ca6
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-17 04:44:57 +0530

    Add Longest-Subsequence-With-Non-Zero-Bitwise-XOR (array)

---
+++ leetcode/array/3702-Longest-Subsequence-With-Non-Zero-Bitwise-XOR.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestSubsequence(vector<int>& nums) {
+        int n = nums.size();
+        int totalXor = 0;
+        bool hasNonZero = false;
+        for (int num : nums) {
+            totalXor ^= num;
+            if (num != 0) {
+                hasNonZero = true;
+            }
+        }
+        if (totalXor != 0) {
+            return n;
+        }
+        if (hasNonZero) {
+            return n - 1;
+        }
+        return 0;
+    }
+};
